08c341dcfeb44705c1ef39381db8885397afade7
Graren <ogcol15@gmail.com>
2018-01-02 22:15:26 +0000

    FOREVERJSPAPOOOO

--- start-state.js
+++ start-state.js
@@ -1,7 +1,88 @@
-const { fork, spawn } = require('child_process');
+// const { fork, spawn } = require('index _process');
+// fork('./test/store/index.js');
+// fork('./test/logger/index.js');
+// fork('./test/index.js');
+const forever = require('forever-monitor');
+process.stdin.resume();
 
+const index  = new(forever.Monitor)('./test/index.js', {
+    max: 5,
+    silent: false,
+    minUptime: 2000,
+    spinSleepTime: 5000
+});
 
-fork('./test/store/index.js');
-fork('./test/logger/index.js');
-fork('./test/index.js');
+const store  = new(forever.Monitor)('./test/store/index.js', {
+    max: 5,
+    silent: false,
+    minUptime: 2000,
+    spinSleepTime: 5000
+});
+
+const logger  = new(forever.Monitor)('./test/logger/index.js', {
+    max: 5,
+    silent: false,
+    minUptime: 2000,
+    spinSleepTime: 5000
+});
+
+index.on('start', function() {
+    console.log('Forever index started for first time.');
+});
+
+index.on('exit', function() {
+    console.error('ind file has exited after '+index.max+' restarts');
+});
+
+index.on('error', () => {
+    process.kill(store.childData.pid);
+    console.error('ind file has errored');    
+})
+
+index.on('stop', () => {
+    process.kill(store.childData.pid);
+    console.error('ind file has been killed');    
+})
+
+store.on('start', function() {
+    console.log('store index started for first time.');
+});
+
+store.on('exit', function() {
+    console.error('store file has exited after '+store.max+' restarts');
+});
+
+logger.on('start', function() {
+    console.log('logger index started for first time.');
+});
+
+logger.on('exit', function() {
+    console.error('store file has exited after '+logger.max+' restarts');
+});
+
+//Exit handler.
+function exitHandler(options, err) {
+    try{
+        //Killing node process manually that is running "Index.js" file.
+        process.kill(index.childData.pid);
+        process.kill(store.childData.pid);
+        process.kill(logger.childData.pid);        
+        console.log("index  process killed succesfully!!");
+        console.log("Forever exit!!");
+    }
+    catch(err){
+        console.log("index  process already stopped!!");
+        console.log("Forever exit!!");
+    }
+
+    //Killing forever process.
+    process.exit();
+}
+
+//Handling user exit events like Ctrl+C.
+process.on('SIGINT', exitHandler.bind(null, {exit: true}));    
+
+index.start();
+store.start()
+logger.start()
 
